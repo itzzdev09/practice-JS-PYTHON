ff040106ad7c20546313562d60dc63ba89710427
Devraj Pal <devved90@gmail.com>
2023-12-03 22:53:34 +0530

    Day1 of harkirats class

---
+++ harkirats class/day1.js
@@ -0,0 +1,75 @@
+// console.log("hello world!");
+// console.log(a);
+
+
+// var a = 1
+// console.log(a);
+
+
+// let a = 1;
+// a = 2
+// console.log(a);
+
+// const a = 1;
+// a = 2
+// console.log(a);
+
+// throws Error
+
+// data types- strings const and bool
+
+// let firstname = "Tesla";
+// let age = 21;
+// let ismarried = false;
+
+// console.log("this persons name is " + firstname + " and his age is " + age)
+
+// if (ismarried == false) {
+//     console.log(firstname + " is not married);
+// }
+// if (ismarried == true) {
+//     console.log(firstname + "is married");
+// }
+
+
+// loops
+
+// let answer = 0;
+// for(let i = 0; i <= 100; i++) {
+//     answer = answer + i
+// }
+// console.log(answer);
+
+
+
+
+// print the even ages in array
+
+
+// const ages = [21,22,23,24,25,26,27,28,29,29]
+
+// for (let i = 0; i < ages.length; i++) {
+//     if (ages[i] % 2 == 0)
+//     console.log(ages[i]);
+// }
+
+
+const person1 = "dev"
+const gender1 = "male"
+
+const person2 = "Tesla"
+const gender2 = "male"
+
+const person3 = "devi"
+const gender3 = "female"
+
+const person4 = "dev"
+const gender4 = "male"
+
+
+or 
+
+const allusers = [{
+    firstname: "devraj",
+    gender: "male" 
+}]
