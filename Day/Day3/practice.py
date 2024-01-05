bb774e5e96c906a33603e2a297d5c85ec3c40c24
Devraj Pal <devved90@gmail.com>
2024-01-05 21:26:58 +0530

    str2int

--- Day/Day3/practice.py
+++ Day/Day3/practice.py
@@ -1,8 +1,17 @@
-#calculate time
-import time
+ #calculate time
+# import time
 
-start = time.time()
-for i in range(1,50):   
-    print(i)
-print(time.time() - start)
+# start = time.time()
+# for i in range(1,50):   
+#     print(i)
+# print(time.time() - start)
 
+
+l = {"dev", "min", "max", "ved"}
+s = {"rohit", "param", "vineet", "sejal", "nikhil", "aryan"}
+print(len(l))
+print(len(s))
+if len(l)>len(s):
+    print("the first list has more values than the second one")
+else:
+    print("the second list has more values than the first")
