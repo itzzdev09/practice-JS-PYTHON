74a7c4676c08e621256336609fad7d1f909d1586
Devraj Pal <devved90@gmail.com>
2024-01-14 20:05:50 +0530

    more functions added

--- harkirats class/day1.js
+++ harkirats class/day1.js
@@ -119,3 +119,31 @@ function getLength(str) {
       }
       findIndexOf("Hello World", "World");
       
+      // lastIndexOf
+      function findLastIndexOf(str, target) {
+        console.log("Original String:", str);
+        console.log("Index:", str.lastIndexOf(target));
+      }
+      findLastIndexOf("Hello World World", "World");
+      
+      // slice
+      function getSlice(str, start, end) {
+        console.log("Original String:", str);
+        console.log("After slice:", str.slice(start, end));
+      }
+      getSlice("Hello World", 0, 5);
+      
+      // substring
+      function getSubstring(str, start, end) {
+        console.log("Original String:", str);
+        console.log("After substring:", str.substring(start, end));
+      }
+      getSubstring("Hello World", 0, 5);
+      
+      // replace
+      function replaceString(str, target, replacement) {
+        console.log("Original String:", str);
+        console.log("After replace:", str.replace(target, replacement));
+      }
+      replaceString("Hello World", "World", "JavaScript");
+      
