4b152254cba71d33dd0ef46459e1795425496156
Devraj Pal <devved90@gmail.com>
2024-01-14 20:10:09 +0530

    date components

---
+++ harkirats class/date.js
@@ -0,0 +1,12 @@
+function dateMethods() {
+    const currentDate = new Date();
+    console.log("Current Date:", currentDate);
+  
+    // Getting various components of the date
+    console.log("Date:", currentDate.getDate());
+    console.log("Month:", currentDate.getMonth() + 1); // Months are zero-indexed, so adding 1
+    console.log("Year:", currentDate.getFullYear());
+    console.log("Hours:", currentDate.getHours());
+    console.log("Minutes:", currentDate.getMinutes());
+    console.log("Seconds:", currentDate.getSeconds());
+  
