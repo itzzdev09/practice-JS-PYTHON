0759dbb2cf0da0ca103c81a0944783fe52bd9626
Devraj Pal <devved90@gmail.com>
2024-01-14 20:10:31 +0530

    date example

--- harkirats class/date.js
+++ harkirats class/date.js
@@ -10,3 +10,19 @@ function dateMethods() {
     console.log("Minutes:", currentDate.getMinutes());
     console.log("Seconds:", currentDate.getSeconds());
   
+    // Setting components of the date
+    currentDate.setFullYear(2022);
+    console.log("After setFullYear:", currentDate);
+  
+    currentDate.setMonth(5); // Setting month to June (zero-indexed)
+    console.log("After setMonth:", currentDate);
+  
+    // Getting and setting time in milliseconds since 1970
+    console.log("Time in milliseconds since 1970:", currentDate.getTime());
+  
+    const newDate = new Date(2023, 8, 15); // Creating a new date
+    console.log("New Date:", newDate);
+  }
+  
+  // Example Usage for Date Methods
+  dateMethods();
