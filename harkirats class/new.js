414c2fbf3dbd0af3ebf0fc26c3b9deb8de40567c
Devraj Pal <devved90@gmail.com>
2024-01-17 23:00:15 +0530

    trial

---
+++ harkirats class/new.js
@@ -0,0 +1,8 @@
+let userinput = prompt("enter a number");
+let num = parseInt(userinput);
+if(a % 2 == 0){
+    console.log(a + " the number is odd"); 
+}
+else{
+    console.log("the number is odd");
+}
