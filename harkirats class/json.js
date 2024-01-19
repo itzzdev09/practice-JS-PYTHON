05f14e4255c2f647b3cba4d5ad21b45e7f7d144f
Devraj Pal <devved90@gmail.com>
2024-01-19 23:53:19 +0530

    stringify

--- harkirats class/json.js
+++ harkirats class/json.js
@@ -4,3 +4,14 @@ function jsonMethods(jsonString) {
     // Parsing JSON string to JavaScript object
     let parsedObject = JSON.parse(jsonString);
     console.log("After JSON.parse():", parsedObject);
+  
+    // Stringifying JavaScript object to JSON string
+    let jsonStringified = JSON.stringify(parsedObject);
+    console.log("After JSON.stringify():", jsonStringified);
+  }
+  
+  // Example Usage for JSON Methods
+  const sampleJSONString =
+    '{"key": "value", "number": 42, "nested": {"nestedKey": "nestedValue"}}';
+  
+  jsonMethods(sampleJSONString);
