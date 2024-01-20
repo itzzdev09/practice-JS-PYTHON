b42f90a7e5aee4ef0be618db68ed72b055aaf9cd
Devraj Pal <devved90@gmail.com>
2024-01-20 23:23:55 +0530

    examples

--- harkirats class/object.js
+++ harkirats class/object.js
@@ -8,3 +8,22 @@ function objectMethods(obj) {
     let values = Object.values(obj);
     console.log("After Object.values():", values);
   
+    let keys = Object.keys(obj);
+    console.log("After Object.keys():", keys);
+  
+    let values = Object.values(obj);
+    console.log("After Object.values():", values);
+  
+    let entries = Object.entries(obj);
+    console.log("After Object.entries():", entries);
+  
+    let hasProp = obj.hasOwnProperty("property");
+    console.log("After hasOwnProperty():", hasProp);
+  
+    let newObj = Object.assign({}, obj, { newProperty: "newValue" });
+    console.log("After Object.assign():", newObj);
+  
+  
+  }
+  
+ 
