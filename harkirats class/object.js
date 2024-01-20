6e91f7a100969c3ff043fbc02c1799d2e98c2369
Devraj Pal <devved90@gmail.com>
2024-01-20 23:23:33 +0530

    obj

--- harkirats class/object.js
+++ harkirats class/object.js
@@ -2,3 +2,9 @@
 function objectMethods(obj) {
     console.log("Original Object:", obj);
   
+    let keys = Object.keys(obj);
+    console.log("After Object.keys():", keys);
+  
+    let values = Object.values(obj);
+    console.log("After Object.values():", values);
+  
