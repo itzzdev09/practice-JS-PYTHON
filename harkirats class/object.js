af40dce81581447e0993d6f5ad613a9e3f3f6b0c
Devraj Pal <devved90@gmail.com>
2024-01-20 23:24:08 +0530

    sample obj

--- harkirats class/object.js
+++ harkirats class/object.js
@@ -25,5 +25,12 @@ function objectMethods(obj) {
   
   
   }
+   // Example Usage for Object Methods
+   const sampleObject = {
+    key1: "value1",
+    key2: "value2",
+    key3: "value3",
+  };
   
+  objectMethods(sampleObject);
  
