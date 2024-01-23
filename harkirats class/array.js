1114cd1b5002b12d2338053c3a581e27db9dfcd8
Devraj Pal <devved90@gmail.com>
2024-01-23 23:04:25 +0530

    pop

--- harkirats class/array.js
+++ harkirats class/array.js
@@ -6,4 +6,10 @@ function pushExample(arr, element) {
     console.log("After push:", arr);
   }
   pushExample([1, 2, 3], 4);
+  // pop()
+  function popExample(arr) {
+    console.log("Original Array:", arr);
   
+    arr.pop();
+    console.log("After pop:", arr);
+  }
