1583dc4c906eb7255bd4464715322b3ac3b70504
Devraj Pal <devved90@gmail.com>
2024-01-23 23:04:55 +0530

    shift

--- harkirats class/array.js
+++ harkirats class/array.js
@@ -13,3 +13,16 @@ function pushExample(arr, element) {
     arr.pop();
     console.log("After pop:", arr);
   }
+  popExample([1, 2, 3]);
+  
+  // shift()
+  function shiftExample(arr) {
+    console.log("Original Array:", arr);
+  
+    arr.shift();
+    console.log("After shift:", arr);
+  }
+  shiftExample([1, 2, 3]);
+  
+  // unshift()
+  
