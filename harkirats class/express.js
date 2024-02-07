8fe635648016908184f58b259383ae96aa0b70fe
Devraj Pal <devved90@gmail.com>
2024-02-07 22:46:57 +0530

    basic exp

--- harkirats class/express.js
+++ harkirats class/express.js
@@ -8,3 +8,11 @@ app.get('/', (req, res) => {
   res.send('Hello, this is the root/main route!');
 });
 
+app.get('/api', (req, res) => {
+    res.json({ message: 'This is the API route.' });
+  });
+  app.get('/greet/:name', (req, res) => {
+    const { name } = req.params;
+    res.send(`Hello, ${name}!`);
+  });
+ 
