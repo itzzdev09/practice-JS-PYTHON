9dd18f2e97462d97e841e4c965388c8facf86a96
Devraj Pal <devved90@gmail.com>
2024-02-12 20:13:11 +0530

    get

--- harkirats class/getreq.js
+++ harkirats class/getreq.js
@@ -7,3 +7,13 @@ app.get('/', (req, res) => {
     res.send('Hello, this is a GET request!');
   });
   
+  // Define a route with a parameter
+  app.get('/greet/:name', (req, res) => {
+    const { name } = req.params;
+    res.send(`Hello, ${name}!`);
+  });
+  
+  // Start the server
+  app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+  });
