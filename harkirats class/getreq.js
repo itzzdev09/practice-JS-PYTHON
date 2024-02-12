80e46959d47574ef72921a09c477adbdc4d50579
Devraj Pal <devved90@gmail.com>
2024-02-12 20:11:56 +0530

    get!!

--- harkirats class/getreq.js
+++ harkirats class/getreq.js
@@ -2,3 +2,8 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+// Define a simple GET route
+app.get('/', (req, res) => {
+    res.send('Hello, this is a GET request!');
+  });
+  
